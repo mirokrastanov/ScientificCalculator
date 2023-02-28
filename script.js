0d51403efe5bf1793976bfa45f034bdb5985814b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-28 21:32:11 +0200

    Added targets for important DOM elements

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function solve() {
-    
+    let input = document.querySelector('#input');
+    let output = document.querySelector('#result');
+    let select = document.querySelector('#selectMenuTo');
+    let button = document.querySelector('#container button');
+    // button.setAttribute("onclick", "solve()");
+    let binaryOption = select.querySelector('option');
+    let hexOption = document.createElement("option");
+  
+   
 
 
 }
