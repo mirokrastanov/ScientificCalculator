969a438d2a440aa2038e81262baf79f1a8b0ef4a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-07 00:36:56 +0300

    bug fix - x2 now working properly

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 import { Calculator } from "./src/class.js";
 
-const board = document.querySelector('.calculator-grid');
+const allBtns = document.querySelectorAll('.calculator-grid > button');
 const currentEl = document.querySelector('.current-operand');
 const previousEl = document.querySelector('.previous-operand');
 const calculator = new Calculator(previousEl, currentEl);
 
-
-board.addEventListener('click', (e) => {
-    e.preventDefault();
-    let target = e.target;
-    if (target.tagName != 'BUTTON') return;
-    console.log(target.textContent);
-    if (target.textContent != Number(target.textContent) && target.textContent != '.') {
-        if (target.textContent == '=') {
-            calculator.compute();
+allBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let target = e.currentTarget;
+        console.log(target.textContent);
+        if (target.textContent != Number(target.textContent) && target.textContent != '.') {
+            if (target.textContent == '=') {
+                calculator.compute();
+                calculator.updateDisplay();
+            } else {
+                calculator.chooseOperation(target.textContent);
+                calculator.updateDisplay();
+            }
+        } else {
+            calculator.appendNumber(target.textContent);
             calculator.updateDisplay();
         }
-        calculator.chooseOperation(target.textContent);
-        calculator.updateDisplay();
-    } else {
-        calculator.appendNumber(target.textContent);
-        calculator.updateDisplay();
-    }
-
+    });
 });
-
